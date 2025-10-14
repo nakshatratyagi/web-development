f4b9ffe208f3e94f8977cd9555b68afc26dac3ee
nakshatra <tyaginakshatra3@gmail.com>
2025-10-14 21:43:11 +0530

    Add HTML files

---
+++ simpleprograms /evenodd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"enter a number";
+    cin>>x;
+    if (x%2==0){
+        cout<<"even";
+    }
+    else {
+        cout<<"odd";
+    }
+    
+}
